8408d49aa2f984c40e98672597701c92dc9b25b3
Jerry Goguette <jerry_g16@hotmail.com>
2017-03-17 19:19:26 -0400

    Initial Commit

---
+++ seneca.js
@@ -0,0 +1,23 @@
+// [INFO] See discussion of node.js exports here:
+// https://www.sitepoint.com/understanding-module-exports-exports-node-js/
+
+/**
+ * Given a string `email`, return `true` if the string is in the form
+ * of a valid Seneca College email address, `false` othewise.
+ */
+exports.isValidEmail = function isValidEmail(email) {
+    // TODO: needs to be implemented
+  const regEx = '(\b[a-zA-Z]+\b|\b[a-zA-Z]+.[a-zA-Z]+\b)@(\bmyseneca\b|\bsenecacollege\b).ca';
+  return email.search(regEx);
+};
+
+/**
+ * Given a string `name`, return a formatted Seneca email address for
+ * this person. NOTE: the email doesn't need to be real/valid/active.
+ */
+exports.formatSenecaEmail = function formatSenecaEmail(name) {
+    // TODO: needs to be implemented
+  const tokens = name.split(' ');
+  const email = `${tokens[0].charAt(0)}${tokens[1]}@myseneca.ca`;
+  return email;
+};
